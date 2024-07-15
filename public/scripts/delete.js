164c023b7aff1b46a62b4926876def0c21004f65
Felipe1995c <159103016+Felipe1995c@users.noreply.github.com>
2024-07-14 19:45:53 -0700

    Added scripts and seeds. Working on templates now

---
+++ public/scripts/delete.js
@@ -0,0 +1,29 @@
+
+const deleteurl = window.location.pathname;
+const deleteid = url.substring(deleteurl.lastIndexOf('/') + 1);
+console.log('delete id' + deleteid);
+
+const deleteArticle = async (event) => {
+    event.preventDefault();
+
+    if (id) {
+        const response = await fetch(`/api/articles/${deleteid}`, {
+        method: "DELETE",
+        body: JSON.stringify({
+            deleteid: id,
+        }),
+        headers: { "Content-Type": "application/json" },
+        });
+
+        if (response.ok) {
+        document.location.replace("/");
+        } else {
+        alert("Failed to delete!");
+        }
+    }
+};
+
+
+document
+.querySelector('.deleteBtn')
+.addEventListener('click', deleteArticle);
