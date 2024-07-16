f0ccf889f4d6c74594619b6096aea74c52ddfbb4
Felipe1995c <159103016+Felipe1995c@users.noreply.github.com>
2024-07-15 19:54:41 -0700

    fixing up code. Need articles to render

--- config/connection.js
+++ config/connection.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const Sequelize = require('sequelize');
 
 
-const sequelize = process.env.techBlog_url
-  ? new Sequelize(process.env.techBlog_url)
+const sequelize = process.env.techBlog_db
+  ? new Sequelize(process.env.techBlog_db)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
       dialect: 'postgres',
       dialectOptions: {
         decimalNumbers: true,
